29c37caf5e70fee9930a5df280fa654dcf47cc85
Jackie Li <jli38@stuy.edu>
2019-02-24 21:15:08 -0500

    tweak

--- main.py
+++ main.py
@@ -33,13 +33,13 @@ print_matrix(matrix2)
 
 clear_screen(screen)
 matrix = new_matrix(0)
-x, y, z = 250, 250, 1
-for i in range(250):
-	add_edge(matrix, x, y, z, int( i * math.sin(i/math.pi)) + 250, int( i * math.cos(i)) + 250 , 1)
+x, y, z = 200, 200, 1
+for i in range(200):
+	add_edge(matrix, x, y, z, int( i * math.sin(i/math.pi)) + 200, int( i * math.cos(i)) + 200 , 1)
 	x = int( i*math.pi ) + random.randrange(50)
 	y = int( i*math.pi ) + random.randrange(50)
 	z = 1
-add_edge(matrix, x, y, z, 250, 250, 1)
+add_edge(matrix, x, y, z, 200, 200, 1)
 draw_lines(matrix,screen,color)
 
 save_extension(screen,"img.png")
