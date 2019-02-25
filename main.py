e9c4916cb08b06e721fe119ef3b5b12c2001b27a
Jackie Li <jli38@stuy.edu>
2019-02-24 21:28:06 -0500

    1

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ print_matrix(matrix2)
 clear_screen(screen)
 matrix = new_matrix(0)
 x, y, z = 100, 100, 1
-for i in range(200):
+for i in range(150):
 	add_edge(matrix, x, y, z, int( i * math.sin(i/math.pi)) + 100, int( i * math.cos(i)) + 100 , 1)
 	x = int( i*math.pi ) + random.randrange(50)
 	y = int( i*math.pi ) + random.randrange(50)
